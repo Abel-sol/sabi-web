2e59bfca4bf30b5a16fe04b3c7829d84219bafaa
Abel Solomon <abelsolab17@gmail.com>
2024-04-24 21:54:29 +0300

    refactor: specified type for authOptions

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -1,16 +1,16 @@
-import NextAuth from "next-auth";
+import NextAuth, { NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import { env } from 'process';
 
-export const authOptions = {
+export const authOptions : NextAuthOptions = {
 
   providers: [
-    
     GoogleProvider({
       clientId: env.GOOGLE_CLIENT_ID as string,
       clientSecret: env.GOOGLE_CLIENT_SECRET as string
-    }),
-
-  ],
+    },
+  ),
+  
+]
 }
 export default NextAuth(authOptions);
