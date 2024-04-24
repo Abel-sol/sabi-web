73e9b051397606d3cd309b8d5c0a777085ba92da
Abel Solomon <abelsolab17@gmail.com>
2024-04-24 15:26:57 +0300

    feat: created a landing page compoent

---
+++ src/components/LandingPage/LandingPage.tsx
@@ -0,0 +1,31 @@
+import Nav from "../NavBarComp/Nav";
+
+const LandingPage = () => {
+
+  return (
+    <div className="flex min-h-screen flex-col bg-gradient-to-t from-white to-indigo-500">
+      <Nav />
+      <div className="flex min-h-screen w-1/2 text-black flex-col  items-start gap-2 pt-28 pl-32">
+        <div className="flex flex-col gap-2">
+
+          <h1 className="text-4xl font-bold">Sabi 🎉</h1>
+          <p className="text-lg  font-semibold">Your one-stop destination for finding where to go out.<br />
+            Book your tickets from the comfort of your home.</p>
+          <div className="flex justify between gap-10 mt-5">
+
+            <button className="bg-blue-500 text-white p-2 rounded-md">
+              Login
+            </button>
+            <button className="bg-blue-500 text-white p-2 rounded-md">
+              Sign Up
+            </button>
+          </div>
+        </div>
+      </div>
+      <div>
+
+      </div>
+    </div>);
+}
+
+export default LandingPage;
