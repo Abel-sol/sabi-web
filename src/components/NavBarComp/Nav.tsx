b8d6e515cd98a42c222cb8cef5789fd3a047a5f3
Abel Solomon <abelsolab17@gmail.com>
2024-04-25 22:29:29 +0300

    style: made ui changes in navbar

--- src/components/NavBarComp/Nav.tsx
+++ src/components/NavBarComp/Nav.tsx
@@ -1,19 +1,20 @@
 "use client"
 import { NavigationMenuData } from "@/data";
+import { useSession } from "next-auth/react";
 import { useState } from "react";
 import { IoMdClose } from "react-icons/io";
 import { IoMenu } from "react-icons/io5";
+import Logo from "./Logo";
 import NavItem from "./NavItem";
 
 export default function Nav() {
+	const session = useSession();
 	const [toggle, setToggle] = useState(false);
 
 	return (
-		<nav className=" flex w-full justify-end items-center px-10 py-5 bg-transparent sticky top-0 z-50 mx-auto">
+		<nav className=" flex w-full justify-end items-center px-10 py-5 bg-indigo-400 sticky top-0 z-50 mx-auto shadow-xl rounded-b-sm">
 			<div className="flex w-full justify-between sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 ">
-				<div className="font-bold text-4xl">
-					Sabi Logo
-				</div>
+				<Logo />
 				<ul className="items-center space-x-8 hidden md:flex text-black">
 					{NavigationMenuData.map((menu, idx) => {
 						return (
@@ -40,7 +41,12 @@ export default function Nav() {
 						className={`${toggle ? "flex" : "hidden"
 							} p-4 bg-indigo-500 absolute top-20 right-0 w-full md:w-auto my-2  rounded-xl sidebar`}
 					>
+						<div>
+
+
+						</div>
 						<ul className="list-none flex flex-col gap-2 justify-end items-start ml-7 flex-1 text-cyan-50">
+
 							{NavigationMenuData.map((menu, idx) => {
 								return (
 									<NavItem key={idx} link={menu.link}>
@@ -48,13 +54,9 @@ export default function Nav() {
 									</NavItem>
 								);
 							})}
-
+							<li>{session && <img src="" alt="profile" className="object-cover w-10 h-10 rounded-full" />}</li>
 						</ul>
 					</div>
-				</div>
-				<div className="flex justify-between gap-10 text-black sm:hidden">
-
-
 				</div>
 			</div>
 		</nav>
