e56647c34c94115fb22eac1717e6c9aa99c9cc8b
Abel Solomon <abelsolab17@gmail.com>
2024-04-25 22:31:52 +0300

    feat: added navbar to users home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
-import ChapaInput from "../components/ChapaInput";
+import Nav from "@/components/NavBarComp/Nav";
 
 export default function Home() {
 
   return (
-    <div>
-
-      <ChapaInput />
+    <div className="flex flex-col  h-screen  bg-gradient-to-t from-white to-indigo-500">
+      <Nav />
+      {/* <ChapaInput /> */}
 
     </div>
   );
