8d8f81cc8446832ed38fda39c6b923938f5e3ce9
Abel Solomon <abelsolab17@gmail.com>
2024-04-28 21:42:44 +0300

    test: added fake data for firestore

---
+++ src/lib/fakeData.ts
@@ -0,0 +1,38 @@
+
+// export async function fakeEvnets() {
+
+
+// 	const restaurantsToAdd = 5;
+	export const data = [
+    {
+      name: "lorem",
+      location: "lorem",
+      photo: "lorem",
+      city: "lorem",
+      category: "lorem",
+      price: 0,
+      // createdAt: serverTimestamp()
+
+    },
+    {
+      name: "lorem2",
+      location: "lorem2",
+      photo: "lorem2",
+      city: "lorem2",
+      category: "lorem2",
+      price: 20,
+      // createdAt: serverTimestamp(),
+
+    },
+    {
+      name: "lorem3",
+      location: "lorem3",
+      photo: "lorem3",
+      city: "lorem3",
+      category: "lorem3",
+      price: 30,
+      // createdAt: serverTimestamp(),
+
+    },
+  ];
+
