f7ed88d094bbc26d9e2cab467babf4f8f5306db3
Abel Solomon <abelsolab17@gmail.com>
2024-04-28 21:42:01 +0300

    fix: bug fixes on get and add data form firestore

--- firebase/firestore.ts
+++ firebase/firestore.ts
@@ -4,22 +4,34 @@ import { db } from "./firebase";
 export async function getEvents(filters = {} ) {
   let q = query(collection(db,"events"));
   // q = applyQueryFilters(q, filters);
+  let querySnapshot;
   try{
 
-    const querySnapshot = await getDocs(q);
+     querySnapshot = await getDocs(q);
 
   } catch (e){
     console.log("There was an error getting the documents")
     console.error("Error getting documents: ", e);
+    return {};
   }
+  // return JSON.stringify(querySnapshot);
+  return querySnapshot.docs.map(doc => {
+		return {
+			id: doc.id,
+			...doc.data(),
+			// Only plain objects can be passed to Client Components from Server Components
+			// timestamp: serverTimestamp(),
+		};
+	});
+
 }
 export async function addEvent() {
   const datas = {name: "test", location: "test Location", photo: "test photo", city: "test city", category: "test category"}//await generateEvent();
 
   try{
 
-    await addDoc(collection(db, "events"), datas);
-    console.log("data added successfully!!!")
+    const docRef = await addDoc(collection(db, "events"), datas);
+    return docRef;
   } catch(e){
 
     console.log("There was an error adding the document")
