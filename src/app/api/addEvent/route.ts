06fa1d684e5c5ac032c3b3afaa9fe5e9e5c4c227
Abel Solomon <abelsolab17@gmail.com>
2024-04-30 08:44:40 +0300

    fix: made addEvent to send data to firestore successfully

--- src/app/api/addEvent/route.ts
+++ src/app/api/addEvent/route.ts
@@ -1,28 +1,29 @@
-import { data } from "@/lib/fakeData";
+
 import { addDoc, collection } from "firebase/firestore";
 import { getServerSession } from "next-auth";
 import { db } from "../../../../firebase/firebase";
 
-export async function POST(request: Request) {
-
+export async function POST(req: Request) {
   const session = await getServerSession();
 
   if (!session) 
     return new Response("Unauthorized", { status: 401 });
+  
+  const data = await req.json();
 
   try{
-    // doc = await addDoc(collection(db, "events"), {
-    //   ...data,
-    // });
-    for (const item of data){
-      await addDoc(collection(db,"events"),item); // only when adding fake data
-    }
+     await addDoc(collection(db, "events"), {
+      ...data,
+    });
+    // for (const item of data){
+    //   await addDoc(collection(db,"events"),item); // only when adding fake data
+    // }
 
   } catch(e) {
 
     console.log("There was an error adding the document")
     console.error("Error adding document: ", e);
-    return new Response("Firebase Error", { status: 500 });
+    return  Response.json({err: "Firebase Error"}, { status: 500 });
 
   }
   
