170c23c6afca5e263942899dfaec18e0b73772e0
Abel Solomon <abelsolab17@gmail.com>
2024-05-10 12:57:37 +0300

    style: made EventListings responsive and added some ui fixes

--- src/components/Events/EventComp/ImageCover.tsx
+++ src/components/Events/EventComp/ImageCover.tsx
@@ -3,7 +3,7 @@ type imageprops = {
   name: string;
 }
 export const ImageCover = ({ image, name }: imageprops) => (
-  <div className="w-full h-full rounded-lg object-cover max-h-[300px] min-h-[300px] relative">
-    <img src={image} alt={name} className="w-full h-full absolute object-cover" />
+  <div className="w-full h-full  object-cover max-h-[300px] min-h-[300px] relative">
+    <img src={image} alt={name} className="w-full h-full absolute object-cover rounded-t-xl" />
   </div>
 );
--- src/components/Events/EventListings.tsx
+++ src/components/Events/EventListings.tsx
@@ -12,13 +12,13 @@ function EventListings({ initialEvents }: props) {
   const [events, setEvent] = useState(initialEvents);
   // console.log(events);
   return (
-    <div className="mx-auto">
-      <ul className="grid grid-cols-3 gap-[40px]">
+    <div >
+      <ul className="w-fit mx-auto grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 justify-items-center justify-center gap-y-20 gap-x-14 mt-10 mb-5">
 
         {events.map((event: EventData) => (
           <EventItem key={event.id}
             event={event}
-            className={"bg-white min-w-[300px] mt-[20px] "} />
+            className={"w-72 bg-white shadow-md  rounded-xl duration-500 hover:scale-100 hover:shadow-xl hover:shadow-indigo-400"} />
         ))}
 
       </ul>
