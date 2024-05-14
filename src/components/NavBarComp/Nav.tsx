cd0c83865e779a476d78e701316040a9222cf92d
Abel Solomon <abelsolab17@gmail.com>
2024-05-14 11:37:11 +0300

    fix: removed the comments and modified the route for tickets page in dropdown

--- src/components/NavBarComp/Nav.tsx
+++ src/components/NavBarComp/Nav.tsx
@@ -1,6 +1,7 @@
 "use client"
 import { SearchIcon } from "lucide-react";
 import { signIn, signOut, useSession } from "next-auth/react";
+import Link from "next/link";
 import { Button } from "../ui/button";
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "../ui/dropdown-menu";
 import { Input } from "../ui/input";
@@ -8,68 +9,7 @@ import { Input } from "../ui/input";
 
 export default function Nav() {
 	const session = useSession();
-	// const [toggle, setToggle] = useState(false);
 
-	// return (
-	// <nav className=" flex w-full justify-end items-center px-10 py-3 pb-0 bg-indigo-400 sticky top-0 z-50 mx-auto shadow-xl rounded-b-sm">
-	// 	<div className="flex w-full justify-between sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 ">
-	// 		{header ? <div className={`${header ?? "hidden"} flex gap-2 items-center`}>
-	// 			<Logo />
-	// 			<h1 className="text-3xl font-bold text-white">{header}</h1>
-	// 		</div> : <Logo />}
-	// 		<ul className="items-center space-x-8 hidden md:flex text-black">
-	// 			{NavigationMenuData.map((menu, idx) => {
-	// 				return (
-	// 					<NavItem key={idx} link={menu.link} primary={true} >
-	// 						{menu.name}
-	// 					</NavItem>
-	// 				);
-	// 			})}
-	// 			<NewEvent />
-	// 			<li>
-	// 				<button className="p-5 bg-blue-400 text-white " onClick={() => signOut()} >signout</button>
-	// 			</li>
-	// 		</ul>
-	// 		<div className="md:hidden flex flex-1 justify-end items-center ">
-
-	// 			{!toggle ? (
-	// 				<IoMenu
-	// 					className="w-6 h-6"
-	// 					onClick={() => setToggle((prev) => !prev)}
-	// 				/>
-	// 			) : (
-	// 				<IoMdClose
-	// 					className="w-6 h-6"
-	// 					onClick={() => setToggle((prev) => !prev)}
-	// 				/>
-	// 			)}
-	// 			<div
-	// 				className={`${toggle ? "flex" : "hidden"
-	// 					} p-4 bg-indigo-500 absolute top-20 right-0 w-full md:w-auto my-2  rounded-xl sidebar`}
-	// 			>
-	// 				<div>
-
-
-	// 				</div>
-	// 				<ul className="list-none flex flex-col gap-2 justify-end items-start ml-7 flex-1 text-cyan-50">
-
-	// 					{NavigationMenuData.map((menu, idx) => {
-	// 						return (
-	// 							<NavItem key={idx} link={menu.link}>
-	// 								{menu.name}
-	// 							</NavItem>
-	// 						);
-	// 					})}
-	// 					<li>{session && <img src="" alt="profile" className="object-cover w-10 h-10 rounded-full" />}</li>
-	// 					{//TODO: add signout button
-	// 					}
-	// 				</ul>
-	// 			</div>
-	// 		</div>
-	// 	</div>
-	// </nav>
-
-	// )
 
 	return (
 		<div className="flex flex-col sticky">
@@ -108,7 +48,11 @@ export default function Nav() {
 						<DropdownMenuContent align="end">
 							<DropdownMenuLabel>My Account</DropdownMenuLabel>
 							<DropdownMenuSeparator />
-							<DropdownMenuItem>Settings</DropdownMenuItem>
+							<Link href={`/tickets/${session.data?.user?.id}`} >
+								<DropdownMenuItem >
+									Tickets
+								</DropdownMenuItem>
+							</Link>
 							<DropdownMenuItem>Support</DropdownMenuItem>
 							<DropdownMenuSeparator />
 							{session ? <DropdownMenuItem onClick={() => signOut()}>Logout</DropdownMenuItem> :
