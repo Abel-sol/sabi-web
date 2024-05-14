ade6f00705400e121bfffb5d0b1c2fac44225054
Abel Solomon <abelsolab17@gmail.com>
2024-05-14 11:36:12 +0300

    feat: added a call back session and and adapter for authOptions

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -1,6 +1,9 @@
+import { FirestoreAdapter } from "@auth/firebase-adapter";
 import NextAuth, { NextAuthOptions } from "next-auth";
+import { Adapter } from "next-auth/adapters";
 import GoogleProvider from "next-auth/providers/google";
 import { env } from 'process';
+import { adminAuth, adminDb } from "../../../../../firebase/firebase-admin";
 
 export const authOptions : NextAuthOptions = {
 
@@ -11,6 +14,29 @@ export const authOptions : NextAuthOptions = {
     },
   ),
   
-]
-}
+],
+callbacks:{
+  session: async ({ session, token }) => {
+    if (session?.user) {
+      if (token.sub){
+        session.user.id = token.sub as string;
+        const firebaseToken = await adminAuth.createCustomToken(token.sub)
+        session.firebasetoken = firebaseToken
+      }
+    }
+    return session;
+  },
+  jwt: async ({ user, token }) => {
+    if (user) {
+      token.sub = user.id;
+    }
+    return token;
+  }
+},
+session: {
+  strategy: "jwt"
+},
+adapter: FirestoreAdapter(adminDb) as Adapter,
+} satisfies NextAuthOptions;
+
 export default NextAuth(authOptions);
