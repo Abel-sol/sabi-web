a9f9b7a96d7ffa3ffdb39325abd5fa6716a76077
Abel Solomon <abelsolab17@gmail.com>
2024-05-17 10:25:36 +0300

    feat: made responsice landing page

--- src/components/LandingPage/LandingPage.tsx
+++ src/components/LandingPage/LandingPage.tsx
@@ -7,13 +7,13 @@ const LandingPage = () => {
   return (
     <div className="flex min-h-screen flex-col bg-gradient-to-t from-white to-indigo-500">
       <Nav />
-      <div className="flex min-h-screen w-1/2 text-black flex-col  items-start gap-2 pt-28 pl-32">
+      <div className="flex min-h-screen flex-wrap w-full lg:w-1/2 text-black flex-col  items-start gap-2 pt-28 pl-32">
         <div className="flex flex-col gap-2">
 
           <h1 className="text-4xl font-bold">Sabi 🎉</h1>
           <p className="text-lg  font-semibold">Your one-stop destination for finding where to go out.<br />
             Book your tickets from the comfort of your home.</p>
-          <div className="flex justify between gap-10 mt-5">
+          <div className="flex justify-center gap-10 mt-5">
             <Button onClick={() => signIn("google", { callbackUrl: "/" })} className="bg-indigo-500 text-white p-2 rounded-xl hover:opacity-75 hover:bg-indigo-500 px-5 duration-75" >
               Sign in
             </Button>
